dac8185b6e191358cfc2361c4e0160fb721d1b18
Admin <Admin@DESKTOP-H7BKF62>
2022-12-24 13:10:20 +0530

    App utilis

---
+++ Orange_practice/src/utilis/Apputilis.java
@@ -0,0 +1,36 @@
+package utilis;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class Apputilis {
+ public static WebDriver driver;
+ public static String url="http://orangehrm.qedgetech.com";
+ 
+ 
+ @BeforeSuite
+	public static void launchApp()
+	{   driver=new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get(url);
+	}
+	
+	@AfterSuite
+	public static void closeApp()
+	{
+		driver.close();
+	}
+	
+	
+	
+	
+	
+	
+	
+}
